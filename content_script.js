e13ed01fe23463c79a9c38b7975547b5e81f13d7
Steven Te <testeven152@gmail.com>
2019-10-01 02:28:05 -0700

    create, join, leave

--- content_script.js
+++ content_script.js
@@ -2,52 +2,58 @@
 
 $(function(){
 
-    var connectionOptions =  {
-        "force new connection" : true,
-        "reconnectionAttempts": "Infinity", //avoid having user reconnect manually in order to prevent dead clients after a server restart
-        "timeout" : 10000,                  //before connect_error and connect_timeout are emitted.
-        "transports" : ["websocket"]
-    };
+    if(!window.ywfLoaded) {
+        ywf = true;
+
+        var connectionOptions =  {
+            "force new connection" : true,
+            "reconnectionAttempts": "Infinity", //avoid having user reconnect manually in order to prevent dead clients after a server restart
+            "timeout" : 10000, //before connect_error and connect_timeout are emitted.
+            "transports" : ["websocket"]
+        };
     
-    const socket = io('http://localhost:3000/', connectionOptions);
+        var socket = io('http://youtubewfriends.herokuapp.com/', connectionOptions);
+    
+        socket.on('connect', function() {
+            console.log('Client connected')
+        });
 
-/*     var userId = NULL;
-    socket.on('userId', function (data) {
-        if(userId == null){
-            userId = data;
-        }
-    }); */
+        var version = null;
+        var sessionId = null;
+        var ownerId = null;
+        var videoId = null;
+    
+        //popup interactions
+        chrome.runtime.onMessage.addListener(
+            function(request, sender, sendResponse) {
 
-    socket.on('connect', function() {
-        console.log('Client connected')
-    });
-
-    //popup interactions
-    chrome.runtime.onMessage.addListener(
-        function(request, sender, sendResponse) {
-            if(request.type == 'getInitData') {
-                
+                if(request.type == ' getInitData') {
+                    sendResponse({});
+                    return;
+                }
+    
+                if(request.type == 'createSession') {
+                    sendResponse({});
+                    return;
+                }
+    
+                if(request.type == 'joinSession') {
+                   sendResponse({});
+                   return;
+                }
+    
+                if(request.type == 'leaveSession') {
+                    sendResponse({});
+                    return;
+                }
             }
+        );
+    
 
-            if(request.type == 'createSession') {
-                socket.emit('createSession', null, function(){
 
-                });
-            }
 
-            if(request.type == 'joinSession') {
-                socket.emit('joinSession', null, function(){
+    }
 
-                });
-            }
-
-            if(request.type == 'leaveSession') {
-                socket.emit('leaveSession', null, function(){
-
-                });
-            }
-        }
-    );
 
 
 });
--- popup.js
+++ popup.js
@@ -2,6 +2,8 @@
 
 $(function(){
 
+    var localSessionId = null;
+    var localUserId = null;
 
     var connectionOptions =  {
         "force new connection" : true,
@@ -12,18 +14,120 @@ $(function(){
 
     var socket = io('http://youtubewfriends.herokuapp.com/', connectionOptions);
 
-    socket.on('connect', function(){ console.log('client connected'); });
+    socket.on('connect', function() {
+        console.log('Client connected')
+    });
+
+    socket.on('userId', function(data) {
+        localUserId = data;
+    });
+
+    socket.on('playpause', function() {
+
+    });
+
+    socket.on('sync', function() {
+
+    });
 
     chrome.tabs.query({
         active: true,
         currentWindow: true
     }, function(tabs) {
 
-        $('#connectbutton').click(function() {
-            socket.emit('connectbutton', function(){});
-            console.log('connect button clicked');
-        })
+        
+
+        var showError = function(err) {
+            $('.error').removeClass('hidden');
+            $('.no-error').addClass('hidden');
+            $('#error-msg').html(err);
+        };
+
+        $('#close-error').click(function() {
+            $('.no-error').removeClass('hidden');
+            $('.error').addClass('hidden');
+        });
+
+        var startSpinning = function() {
+            // $('#control-lock').prop('disabled', true);
+            $('#create-session').prop('disabled', true);
+            $('#leave-session').prop('disabled', true);
+        };
+
+        var stopSpinning = function() {
+            // $('#control-lock').prop('disabled', false);
+            $('#create-session').prop('disabled', false);
+            $('#leave-session').prop('disabled', false);
+        };
 
 
+        var showConnected = function(sessionId) {
+            // var urlWithSessionId = tabs[0].url.split('?')[0] + '&ywfId=' + encodeURIComponent(sessionId);
+            $('.disconnected').hide();
+            $('.connected').show();
+            $('#share-url').val(sessionId).focus().select();
+        };
+
+        var showDisconnected = function() {
+            $('.disconnected').show();
+            $('.connected').hide();
+            // $('#control-lock').prop('checked', false);
+        };
+
+        $('.error').hide();
+        showDisconnected();
+
+        $('#create-session').click(function() {
+            socket.emit('createSession', null, function(sessionId) {
+                localSessionId = sessionId;
+            });
+            showConnected(localSessionId);
+        });
+
+        $('#join-session').click(function() {
+            if($('#join-id').val() == '') {
+                // do nothing
+            } else {
+                var joinroomid = $('#join-id').val();
+                socket.emit('joinSession', joinroomid, function() {
+
+                });
+                showConnected(joinroomid);
+            }
+        });
+
+        $('#leave-session').click(function() {
+            socket.emit('leaveSession', null, function(){
+                localSessionId = null;
+                localUserId = null;
+            });
+            showDisconnected();
+        });
+
+        $('#play-pause-btn').click(function() {
+            socket.emit('playpausebtn', null, function() {
+
+            });
+        });
+
+        $('#sync-btn').click(function() {
+            socket.emit('syncbutton', null, function() {
+
+            });
+        });
+
+        $('#share-url').click(function(e) {
+            e.stopPropagation();
+            e.preventDefault();
+            $('#share-url').select();
+        });
+
+        $('#copy-btn').click(function(e) {
+            e.stopPropagation();
+            e.preventDefault();
+            $('#share-url').select();
+            document.execCommand('copy');
+        });
+    
     });
 });
